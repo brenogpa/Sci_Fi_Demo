381759779612229c480a98e4aa18609d702c5818
brenogpa <brenogpa@gmail.com>
2022-04-08 12:00:38 -0300

    MuzzleFlash

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
     private float _gravity = 9.81f;
+
+    [SerializeField]
+    private GameObject _muzzleFlash;
     
     // Start is called before the first frame update
     void Start()
@@ -20,8 +23,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
+            _muzzleFlash.SetActive(true);
             Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hitInfo;
 
@@ -29,6 +33,10 @@ public class Player : MonoBehaviour
             {
                 Debug.Log("Hit " + hitInfo.transform.name);
             }
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
